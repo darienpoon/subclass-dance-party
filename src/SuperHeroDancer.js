41d21b2b1c48d1b72c07ce91440bfb85b9fba3ac
WarrenLynes <warrenlynes@gmail.com>
2023-05-23 17:55:07 -0600

    Lineup function

--- src/SuperHeroDancer.js
+++ src/SuperHeroDancer.js
@@ -3,6 +3,12 @@ var SuperHeroDancer = function (top, left, timeBetweenSteps) {
 
   this.direction = getRandomInt(1, 2) === 2 ? 'right' : 'left';
   this.speed = getRandomInt(1, 5);
+  this.lineup = false;
+
+
+  this.$node = $('<img src="src/SuperHeroDancer_Image.png">');
+  this.$node.addClass('dancer');
+  this.$node.addClass('superHeroDancer');
 
   Dancer.prototype.step.call(this);
 };
@@ -10,21 +16,30 @@ var SuperHeroDancer = function (top, left, timeBetweenSteps) {
 SuperHeroDancer.prototype = Object.create(Dancer.prototype);
 SuperHeroDancer.prototype.constructor = SuperHeroDancer;
 SuperHeroDancer.prototype.step = function () {
-  Dancer.prototype.step.call(this);
+  if (this.lineup) {
+    Dancer.prototype.setPosition.call(this, this.top, this.left);
+    this.$node.removeClass('flip-horizontally');
+
+    return;
+  } else {
+    Dancer.prototype.step.call(this);
+  }
 
   if (this.direction === 'right') {
     this.left = this.left + this.speed;
   } else {
+    this.$node.addClass('flip-horizontally');
     this.left = this.left - this.speed;
   }
 
   if (this.left <= 0) {
     this.direction = 'right';
-  } else if (this.left >= $('body').width()) {
+    this.$node.removeClass('flip-horizontally');
+
+  } else if (this.left >= $('body').width() - this.$node.width()) {
+    this.$node.addClass('flip-horizontally');
     this.direction = 'left';
   }
-
-  console.log($('body').width());
 
   Dancer.prototype.setPosition.call(this, this.top, this.left);
 };
